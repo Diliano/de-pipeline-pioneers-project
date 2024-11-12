13fbf05112a3069d159c9e145a759084ce406b6d
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-12 12:22:43 +0000

    build s3.tf resource block for the ingestion lambda code

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -1,15 +1,31 @@
 resource "aws_s3_bucket" "data_bucket" {
-    # S3 bucket for the data. 
-    bucket_prefix = "nc-${var.data_bucket_prefix}"
-    tags = {
-        Name = "DataBucket"
-    }
+  # S3 bucket for the data. 
+  bucket_prefix = "nc-${var.data_bucket_prefix}"
+  tags = {
+    Name = "DataBucket"
+  }
 }
 
 resource "aws_s3_bucket" "code_bucket" {
-    # S3 bucket for the lambda code. 
-    bucket_prefix = "nc-${var.code_bucket_prefix}"
-    tags = {
-        Name = "CodeBucket"
-    }
+  # S3 bucket for the lambda code. 
+  bucket_prefix = "nc-${var.code_bucket_prefix}"
+  tags = {
+    Name = "CodeBucket"
+  }
 }
+
+# ==========================================
+# Ingestion Lambda
+# ==========================================
+
+# ========
+# CREATE
+# ========
+
+# Ingestion lambda code
+resource "aws_s3_object" "ingestion_lambda_code" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "ingestion_lambda/ingestion_lambda.zip"
+  source = data.archive_file.ingestion_lambda.output_path
+}
+
