ffd4a53f36333ef75b50413959bb11db0da9295b
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-12 13:36:22 +0000

    build iam.tf data block for ingestion lambda cloudwatch policy doc

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -30,3 +30,22 @@ data "aws_iam_policy_document" "s3_ingestion_policy_doc" {
     resources = ["${aws_s3_bucket.ingestion_bucket.arn}/*"] # confirm - name taken from 'feature/task2.2-ingestion-s3-bucket' branch
   }
 }
+
+# Ingestion lambda cloudwatch policy doc
+data "aws_iam_policy_document" "ingestion_cw_document" {
+  statement {
+    effect = "Allow"
+
+    actions = ["logs:CreateLogGroup"]
+
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.lambda_ingestion}:*"]
+  }
+}
