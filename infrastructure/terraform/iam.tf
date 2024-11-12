508b9af994f9e45d56c25070d01f2b0b081a2b26
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-12 13:29:30 +0000

    build iam.tf data block for the ingestion lambda s3 write policy doc

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -6,7 +6,7 @@
 # DEFINE
 # ========
 
-# Ingestion lambda trust policy
+# Ingestion lambda trust policy doc
 data "aws_iam_policy_document" "ingestion_trust_policy" {
   statement {
     effect = "Allow"
@@ -17,5 +17,16 @@ data "aws_iam_policy_document" "ingestion_trust_policy" {
     }
 
     actions = ["sts:AssumeRole"]
+  }
+}
+
+# Ingestion lambda - s3 write policy doc
+data "aws_iam_policy_document" "s3_ingestion_policy_doc" {
+  statement {
+    effect = "Allow"
+
+    actions = ["s3:PutObject"]
+
+    resources = ["${aws_s3_bucket.ingestion_bucket.arn}/*"] # confirm - name taken from 'feature/task2.2-ingestion-s3-bucket' branch
   }
 }
