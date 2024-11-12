19f6ddf8ed59cc674baafd97e9b18beb6532d6bb
Wisdom Ebohoin <wisdom_e@hotmail.co.uk>
2024-11-12 12:12:16 +0000

    adding s3 variables for ingestion bucket

--- infrastructure/terraform/vars.tf
+++ infrastructure/terraform/vars.tf
@@ -21,3 +21,8 @@ variable "python_runtime" {
   type    = string
   default = "python3.12"
 }
+
+variable "ingestion_bucket_prefix" {
+  type    = string
+  default = "pipeline-pioneers-ingestion"
+}
