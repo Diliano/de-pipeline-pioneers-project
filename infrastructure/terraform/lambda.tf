7c4f3eed01c1170f798fb33518de814c0fd64dfd
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-12 12:20:30 +0000

    refactor tf data blocks for the ingestion lambda to use placeholder filepaths/filenames

--- infrastructure/terraform/lambda.tf
+++ infrastructure/terraform/lambda.tf
@@ -10,16 +10,16 @@
 data "archive_file" "ingestion_lambda" {
   type             = "zip"
   output_file_mode = "0666"
-  source_file      = "${path.module}/../../ingestion/lambda-function/???" # confirm path and filename 
-  output_path      = "${path.module}/../../ingestion/lambda-function/???" # confirm path and filename based on above filename
+  source_file      = "${path.module}/../../ingestion/lambda-function/ingestion_lambda.py"  # arbitrary path / filename used
+  output_path      = "${path.module}/../../ingestion/lambda-function/ingestion_lambda.zip" # arbitrary path / filename used
 }
 
 # Ingestion layer dependencies
 data "archive_file" "ingestion_layer" {
   type             = "zip"
   output_file_mode = "0666"
-  source_dir       = "${path.module}/../../ingestion/lambda-function/layer"               # suggested path
-  output_path      = "${path.module}/../../ingestion/lambda-function/ingestion_layer.zip" # suggested filename
+  source_dir       = "${path.module}/../../ingestion/lambda-function/layer"               # arbitrary path used
+  output_path      = "${path.module}/../../ingestion/lambda-function/ingestion_layer.zip" # arbitrary filename used
 }
 
 
