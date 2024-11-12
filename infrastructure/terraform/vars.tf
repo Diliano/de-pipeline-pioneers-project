4bee32244878588da688a3cb6b88a0a6005f19aa
Nimmo-san <nat25oloman@gmail.com>
2024-11-12 10:15:27 +0000

    basic look-up for common vars

---
+++ infrastructure/terraform/vars.tf
@@ -0,0 +1,23 @@
+variable "data_bucket_prefix" {
+  type    = string
+  default = "pipeline-pioneers-data"
+}
+
+
+variable "code_bucket_prefix" {
+  type    = string
+  default = "pipeline-pioneers-code"
+}
+
+variable "lambda_ingestion" {
+  type    = string
+  default = "ingestion_lambda"
+}
+
+# lambda_transformation
+# lambda_loading
+
+variable "python_runtime" {
+  type    = string
+  default = "python3.12"
+}
