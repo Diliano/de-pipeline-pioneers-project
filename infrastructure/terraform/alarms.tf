c6b1536ba9c3967a25c1b5307580adb3a570ffc7
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-13 15:07:12 +0000

    build alarms.tf resource block to setup the ingestion lambda alarm email subscription

--- infrastructure/terraform/alarms.tf
+++ infrastructure/terraform/alarms.tf
@@ -28,3 +28,10 @@ resource "aws_sns_topic" "ingestion_alarm_topic" {
   name = "ingestion-lambda-alarm-topic"
 }
 
+# Ingestion lambda alarm - email setup
+resource "aws_sns_topic_subscription" "ingestion_alarm_email_subscription" {
+  topic_arn = aws_sns_topic.ingestion_alarm_topic.arn
+  protocol  = "email"
+  endpoint  = "" # Create an email for us all to use?
+}
+
