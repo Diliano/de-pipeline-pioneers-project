95034a2b0ec420ff38b1cfa722267dabd93475d9
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-13 10:34:35 +0000

    update events.tf comment

--- infrastructure/terraform/events.tf
+++ infrastructure/terraform/events.tf
@@ -6,7 +6,23 @@
 # CREATE
 # ========
 
+# Ingestion lambda scheduler
 resource "aws_cloudwatch_event_rule" "ingestion_scheduler" {
   description         = "Trigger the ingestion lambda every 15 minutes"
   schedule_expression = "rate(15 minutes)"
+}
+
+
+
+
+resource "aws_cloudwatch_event_target" "target_lambda" {
+  rule      = aws_cloudwatch_event_rule.scheduler.name
+  target_id = "QuoteHandlerLambda"
+  arn       = aws_lambda_function.quote_handler.arn
+}
+
+resource "aws_cloudwatch_event_target" "target_lambda" {
+  rule      = aws_cloudwatch_event_rule.scheduler.name
+  target_id = "QuoteHandlerLambda"
+  arn       = aws_lambda_function.quote_handler.arn
 }
