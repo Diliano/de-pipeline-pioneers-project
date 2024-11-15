4195dfff2455f222e1e9d9b002016f3ee01ad6ca
Nimmo-san <nat25oloman@gmail.com>
2024-11-15 10:20:25 +0000

    fixed failing test lambda handler

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -255,30 +255,26 @@ def test_lambda_handler_success(
 
     result = lambda_handler({}, {})
 
-    for table_name in SAMPLE_TABLES_DATA.keys():
-        expected_key = f"{table_name}/{table_name}_{timestamp}.json"
+    for table_name, table_data in SAMPLE_TABLES_DATA.items():
+        expected_key = f"ingestion/{table_name}/{table_name}_{timestamp}.json"
         mock_s3_client.put_object.assert_any_call(
             Bucket="test_bucket",
             Key=expected_key,
-            Body=json.dumps(SAMPLE_TABLES_DATA[table_name]),
+            Body=json.dumps(table_data),
         )
     assert result == {
         "status": "Success",
         "message": "All data ingested successfully",
     }
-    assertion_key = "table1/table1_2023-01-01-00-00-00.json"
-    assert (
-        f"Successfully wrote table1 data to S3 key: {assertion_key}"
-        in caplog.text
-    )
-    assertion_key = "table2/table2_2023-01-01-00-00-00.json"
-    assert (
-        f"Successfully wrote table2 data to S3 key: {assertion_key}"
-        in caplog.text
-    )
+    # Assert logs for successful writes
+    for table_name in SAMPLE_TABLES_DATA.keys():
+        assertion_key = f"ingestion/{table_name}/{table_name}_{timestamp}.json"
+        assert (
+            f"Successfully wrote {table_name} data to S3 key: {assertion_key}"
+            in caplog.text
+        )
 
 
-# @pytest.mark.xfail
 @patch("src.ingestion.S3_INGESTION_BUCKET", "test_bucket")
 @patch("src.ingestion.fetch_tables")
 @patch("src.ingestion.s3_client")
@@ -308,7 +304,7 @@ def test_lambda_handler_partial_failure(
 
     # Ensuring that an error was logged for the table that failed
     assert "Failed to write data to S3" in caplog.text
-    assertion_key = "table2/table2_2023-01-01-00-00-00.json"
+    assertion_key = "ingestion/table2/table2_2023-01-01-00-00-00.json"
     assert (
         f"Successfully wrote table2 data to S3 key: {assertion_key}"
         in caplog.text
