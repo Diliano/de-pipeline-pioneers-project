501c515343a9705bf0ea2049b8a14e0fcbee7ff7
Nimmo-san <nat25oloman@gmail.com>
2024-11-16 15:54:45 +0000

    reformatted to be pep8 compliant, after changes due to the change of timestamp prefix

--- tests/test_lambda_handler.py
+++ tests/test_lambda_handler.py
@@ -8,21 +8,19 @@ from src.ingestion import (
 # @pytest.mark.xfail
 @patch("src.ingestion.fetch_tables")
 @patch("src.ingestion.s3_client.put_object")
-@patch(
-    "src.ingestion.S3_INGESTION_BUCKET",
-    "test_bucket"
-)
+@patch("src.ingestion.S3_INGESTION_BUCKET", "test_bucket")
 @patch("src.ingestion.datetime")
 def test_lambda_handler_success(
-    mock_datetime, mock_put_object, mock_fetch_tables, caplog
+    mock_datetime,
+    mock_put_object,
+    mock_fetch_tables,
+    caplog,
+    sample_table_data,
 ):
     mock_datetime.now.return_value = datetime(2023, 1, 1, 12, 0, 0)
     timestamp = "2023-01-01T12:00:00Z"
 
-    mock_fetch_tables.return_value = {
-        "table1": [{"id": 1, "value": "data1"}],
-        "table2": [{"id": 2, "value": "data2"}],
-    }
+    mock_fetch_tables.return_value = sample_table_data
 
     # Call the lambda_handler function
     result = lambda_handler({}, {})
@@ -36,12 +34,12 @@ def test_lambda_handler_success(
     mock_put_object.assert_any_call(
         Bucket="test_bucket",
         Key=f"ingestion/table1/2023/01/01/table1_{timestamp}.json",
-        Body='[{"id": 1, "value": "data1"}]',
+        Body='[{"id": 1, "value": "test1"}]',
     )
     mock_put_object.assert_any_call(
         Bucket="test_bucket",
         Key=f"ingestion/table2/2023/01/01/table2_{timestamp}.json",
-        Body='[{"id": 2, "value": "data2"}]',
+        Body='[{"id": 2, "value": "test1"}]',
     )
 
     assert "Successfully wrote table1 data to S3 key" in caplog.text
@@ -53,17 +51,18 @@ def test_lambda_handler_success(
 @patch("src.ingestion.s3_client.put_object")
 @patch("src.ingestion.datetime")
 def test_lambda_handler_partial_failure(
-    mock_datetime, mock_put_object, mock_fetch_tables, caplog
+    mock_datetime,
+    mock_put_object,
+    mock_fetch_tables,
+    caplog,
+    sample_table_data,
 ):
     # Mock the current timestamp
     mock_datetime.now.return_value = datetime(2023, 1, 1, 12, 0, 0)
     # timestamp = "2023-01-01T12:00:00Z"
 
     # Mocking fetch_tables to return data for all tables
-    mock_fetch_tables.return_value = {
-        "table1": [{"id": 1, "value": "data1"}],
-        "table2": [{"id": 2, "value": "data2"}],
-    }
+    mock_fetch_tables.return_value = sample_table_data
 
     # Simulating S3 upload failure for table1
     mock_put_object.side_effect = [
@@ -86,16 +85,15 @@ def test_lambda_handler_partial_failure(
 # @pytest.mark.xfail
 @patch("src.ingestion.fetch_tables")
 @patch("src.ingestion.s3_client.put_object")
-@patch(
-    "src.ingestion.S3_INGESTION_BUCKET",
-    "test_bucket"
-)
+@patch("src.ingestion.S3_INGESTION_BUCKET", "test_bucket")
 @patch("src.ingestion.datetime")
-def test_lambda_handler_empty_table(mock_datetime, mock_put_object, mock_fetch_tables, caplog):
+def test_lambda_handler_empty_table(
+    mock_datetime, mock_put_object, mock_fetch_tables, caplog
+):
     mock_datetime.now.return_value = datetime(2023, 1, 1, 12, 0, 0)
     timestamp = "2023-01-01T12:00:00Z"
 
-    # Mocking fetch_tables to return an empty table1 
+    # Mocking fetch_tables to return an empty table1
     # and valid data for table2
     mock_fetch_tables.return_value = {
         "table1": [],
