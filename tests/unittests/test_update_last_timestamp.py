c8c2301cd022d474908f796b8457882c068863b3
Nimmo-san <nat25oloman@gmail.com>
2024-11-18 19:21:24 +0000

    refactored/minor chnages following the addition of sub folder utils

--- tests/unittests/test_update_last_timestamp.py
+++ tests/unittests/test_update_last_timestamp.py
@@ -1,22 +1,25 @@
 from unittest.mock import patch
 from datetime import datetime
-from src.ingestion import update_last_ingestion_timestamp, TIMESTAMP_FILE_KEY
+from src.ingestion import TIMESTAMP_FILE_KEY
 import json
 
+from src.utils.utils import (
+    update_last_ingestion_timestamp,
+)
 
 # @pytest.mark.xfail
-@patch("src.ingestion.S3_INGESTION_BUCKET", "test_bucket")
-@patch("src.ingestion.s3_client")
-def test_update_last_ingestion_timestamp(mock_s3_client):
+@patch("src.utils.utils.S3_INGESTION_BUCKET", "test_bucket")
+@patch("src.ingestion.s3_client.put_object")
+def test_update_last_ingestion_timestamp(mock_put_object):
     current_timestamp = datetime.now().isoformat()
 
-    with patch("src.ingestion.datetime") as mock_datetime:
+    with patch("src.utils.utils.datetime") as mock_datetime:
         mock_datetime.now.return_value = datetime.fromisoformat(
             current_timestamp
         )
         update_last_ingestion_timestamp()
 
-    mock_s3_client.put_object.assert_called_once_with(
+    mock_put_object.assert_called_once_with(
         Bucket="test_bucket",
         Key=TIMESTAMP_FILE_KEY,
         Body=json.dumps({"timestamp": current_timestamp}),
