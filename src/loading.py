2385a05f689289272fbc8915f47d9e492adaed56
Nimmo-san <nat25oloman@gmail.com>
2024-11-19 09:49:10 +0000

    fixed failing pep8 compliance tests

--- src/loading.py
+++ src/loading.py
@@ -9,6 +9,6 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     logger.info("Loading lambda: %s", json.dumps(event))
 
-    # Placeholder for loading logic 
+    # Placeholder for loading logic
 
     return {"status": "Success"}
