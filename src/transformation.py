da46eac1dde82f484ddf6e478c1c66e17de28f97
Nimmo-san <nat25oloman@gmail.com>
2024-11-19 12:02:55 +0000

    basic layout of transformation functionality, mvp/complete datewarehouse

--- src/transformation.py
+++ src/transformation.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import logging
 import boto3
 
-# import pandas as pd
+import pandas as pd
 import json
 import os
 
@@ -19,6 +19,7 @@ S3_PROCESSED_BUCKET = os.getenv("S3_PROCESSED_BUCKET")
 
 
 def load_data_from_s3(key):
+    """"""
     response = s3_client.get_object(Bucket=S3_INGESTION_BUCKET, Key=key)
     logger.info(f"s3 replied with response {response}")
     if "Body" in response:
@@ -26,9 +27,90 @@ def load_data_from_s3(key):
     return data
 
 
+def create_dim_date(date_data):
+    """
+    Creates dim_date table from a list of datetime strings.
+
+    ARGS
+        dates
+    returns
+        a reformatted 
+    """
+    pass
+
+
+def transform_dim_counterparty(counterpart_data):
+    """
+    Transforms counterparty data into dim_counterparty.
+    """
+    pass
+
+
+def transform_dim_currency(currency_data):
+    """
+    Transforms currency data into dim_currency.
+    """
+    pass
+
+
+def transform_dim_staff(staff_data):
+    """
+    Transforms staff data into dim_staff.
+    """
+    pass
+
+
+def transform_dim_design(design_data):
+    """
+    Transforms design data into dim_design.
+    """
+    pass
+
+
+def transform_dim_location(address_data):
+    """
+    Transforms address data into dim_location.
+    """
+    pass
+
+
+def transform_dim_transaction(transaction_data):
+    """
+    Transforms transaction data into dim_transaction.
+    """
+    pass
+
+
+def transform_dim_payment_type(payment_type_data):
+    """Transforms payment type data into dim_payment_type."""
+    pass
+
+
+def transform_fact_sales_order(transactions, dim_date):
+    """
+    Transforms sales transactions into fact_sales_order.
+    """
+    pass
+
+
+def transform_fact_purchase_orders(transactions, dim_date):
+    """
+    Transforms purchase transactions into fact_purchase_orders.
+    """
+    pass
+
+
+def transform_fact_payment(payments, dim_date, payment_types):
+    """
+    Transforms payment data into fact_payment.
+    """
+    pass
+
+
 def transform_data(data):
     """"""
-    # df = pd.DataFrame(data=data)
+    df = pd.DataFrame(data=data)
+
     # return {
     #     "fact_sales_order": fact_sales_order,
     #     "dim_date": dim_date,
