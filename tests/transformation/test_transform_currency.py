99094cb1c01a7a6609a15aa8f261589b6f7989b3
Wisdom Ebohoin <wisdom_e@hotmail.co.uk>
2024-11-21 11:56:58 +0000

    no data test

--- tests/transformation/test_transform_currency.py
+++ tests/transformation/test_transform_currency.py
@@ -3,6 +3,16 @@ import pandas as pd
 from datetime import datetime
 import pandas.testing as test
 
+def test_empty_currency_data():
+    # Arrange
+    currency_data = []
+
+    # Act
+    result = transform_dim_currency(currency_data)
+    
+    # Assert
+    assert result == None
+
 
 def test_transform_currency_success():
     # Arrange
@@ -26,6 +36,38 @@ def test_transform_currency_success():
             "currency_id": [1, 2],
             "currency_code": ["GBP", "USD"],
             "currency_name": ["British Pound", "US Dollar"],
+        }
+    )
+
+    # Act
+    result = transform_dim_currency(currency_data)
+
+    # Assert
+    test.assert_frame_equal(result, expected_output)
+
+
+def test_for_duplicate_currencys():
+    # Arrange
+    currency_data = [
+        [
+            1,
+            "GBP",
+            datetime(2022, 11, 3, 14, 20, 49),
+            datetime(2022, 11, 3, 14, 20, 49),
+        ],
+        [
+            1,
+            "GBP",
+            datetime(2022, 11, 3, 14, 20, 49),
+            datetime(2022, 11, 3, 14, 20, 49),
+        ],
+    ]
+
+    expected_output = pd.DataFrame(
+        {
+            "currency_id": [1],
+            "currency_code": ["GBP"],
+            "currency_name": ["British Pound"],
         }
     )
 
@@ -68,33 +110,3 @@ def test_for_unknown_currency():
     test.assert_frame_equal(result, expected_output)
 
 
-def test_for_duplicate_currencys():
-    # Arrange
-    currency_data = [
-        [
-            1,
-            "GBP",
-            datetime(2022, 11, 3, 14, 20, 49),
-            datetime(2022, 11, 3, 14, 20, 49),
-        ],
-        [
-            1,
-            "GBP",
-            datetime(2022, 11, 3, 14, 20, 49),
-            datetime(2022, 11, 3, 14, 20, 49),
-        ],
-    ]
-
-    expected_output = pd.DataFrame(
-        {
-            "currency_id": [1],
-            "currency_code": ["GBP"],
-            "currency_name": ["British Pound"],
-        }
-    )
-
-    # Act
-    result = transform_dim_currency(currency_data)
-
-    # Assert
-    test.assert_frame_equal(result, expected_output)
