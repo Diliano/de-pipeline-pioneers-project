53ebef2b52beeb5dbac2a125fb49f12a47e1efb1
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-22 10:51:27 +0000

    remove unused constants from loading.py as they are used in db_utils.py instead

--- src/loading/code/loading.py
+++ src/loading/code/loading.py
@@ -7,16 +7,6 @@ logger.setLevel(logging.INFO)
 
 s3_client = boto3.client("s3", region_name="eu-west-2")
 
-TABLES = {
-    "dim_date": ["date_id"],
-    "dim_staff": ["staff_id"],
-    "dim_location": ["location_id"],
-    "dim_currency": ["currency_id"],
-    "dim_design": ["design_id"],
-    "dim_counterparty": ["counterparty_id"],
-    "fact_sales_order": ["sales_record_id"],
-}
-
 
 def lambda_handler(event, context):
     pass
