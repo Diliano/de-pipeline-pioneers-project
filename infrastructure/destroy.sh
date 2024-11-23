385a1d5bc596636be33a4624f0e3fac04c6911cb
Nimmo-san <nat25oloman@gmail.com>
2024-11-23 23:10:28 +0000

    added destory bash file for ease of use

--- infrastructure/destroy.sh
+++ infrastructure/destroy.sh
@@ -6,6 +6,8 @@ set -e
 # Variables
 PROJECT_NAME="de-pipeline-pioneers"
 TERRAFORM_DIR="./terraform"
+S3_INGESTION_BUCKET="not-sure-yet"
+S3_PROCESSED_BUCKET="not-sure-yet"
 
 
 # 
@@ -25,6 +27,19 @@ echo "👉 Terraform available!"
 # Navigating to Terraform Dir
 echo "👉 Initialising Terraform!"
 cd $TERRAFORM_DIR
+
+
+# Removing objects from S3 Buckets
+echo "👉 Cleaning up S3 Bucket: $S3_INGESTION_BUCKET..."
+aws s3 rm s3://$S3_INGESTION_BUCKET --recursive
+aws s3 rb s3://$S3_INGESTION_BUCKET 
+
+echo "👉 Cleaning up S3 Bucket: $S3_PROCESSED_BUCKET..."
+aws s3 rm s3://$S3_PROCESSED_BUCKET --recursive
+aws s3 rb s3://$S3_PROCESSED_BUCKET
+
+echo "✅ Bucket Cleanup Complete!"
+
 
 # Destroy Terraform-managed Resources
 echo "👉 Destroying Terraform-managed Resources..."
